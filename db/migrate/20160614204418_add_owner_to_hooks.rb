6e9923a5fcd50bb6cda7cc01942950052e0098db
Esteban Arango Medina <marranoparael31@gmail.com>
2018-01-17 09:38:54 -0500

    Update 20160614204418_add_owner_to_hooks.rb

--- db/migrate/20160614204418_add_owner_to_hooks.rb
+++ db/migrate/20160614204418_add_owner_to_hooks.rb
@@ -1,4 +1,4 @@
-class AddOwnerToHooks < ActiveRecord::Migration
+class AddOwnerToHooks < ActiveRecord::Migration[5.0]
   def change
     add_column :zapier_rest_hooks_hooks, :owner_id, :string, default: 0
     add_column :zapier_rest_hooks_hooks, :owner_class_name, :string, default: 'Struct::ZapierApp'
