11e2f21e5950863a274144d1e3ef52f52abcf21a
Celso Pinto <celso@celsopinto.com>
2019-01-05 10:09:34 +0000

    Zapier does not work with CSRF

--- app/controllers/zapier_rest_hooks/application_controller.rb
+++ app/controllers/zapier_rest_hooks/application_controller.rb
@@ -1,5 +1,4 @@
 module ZapierRestHooks
-  class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+  class ApplicationController < ActionController::API
   end
 end
