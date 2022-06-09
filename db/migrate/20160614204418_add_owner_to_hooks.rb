782cab43620d147048ac1dc79f53204e28ecd8de
Shailesh Kalamkar <shailesh.kalamkar@gmail.com>
2022-06-09 21:01:03 +0530

    Updated migrations for Rails 6.1

--- db/migrate/20160614204418_add_owner_to_hooks.rb
+++ db/migrate/20160614204418_add_owner_to_hooks.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddOwnerToHooks < ActiveRecord::Migration[6.0]
+class AddOwnerToHooks < ActiveRecord::Migration[6.1]
   def change
     add_column :zapier_rest_hooks_hooks, :owner_id, :string, default: 0
     add_column :zapier_rest_hooks_hooks, :owner_class_name, :string, default: 'Struct::ZapierApp'
